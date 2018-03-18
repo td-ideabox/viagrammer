abb2fa32d85cab8192e18a60ced66a22c16ac4ef
Trevor Dilley <trevor@twentyideas.com>
2018-03-17 17:33:44 -0700

    Clean up imports in Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,12 @@
 module Main exposing (..)
 
 import AnimationFrame exposing (..)
-import Array exposing (..)
-import Char exposing (..)
-import Debug exposing (..)
-import Dict exposing (..)
 import Keyboard exposing (..)
-import Physics exposing (attract, direction, distance, moveTowards, repulse, sumForces)
-import String exposing (toInt)
 import Task
 import Time exposing (Time)
 import Window
 import Html
 import Html.Styled exposing (..)
-import Svg
-import Svg.Styled exposing (..)
-import Svg.Styled.Attributes exposing (..)
-import Rng exposing (next)
 import View exposing (..)
 import State exposing (..)
 import Types exposing (..)
