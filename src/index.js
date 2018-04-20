701daaca8c8e66076c4d6b2384da21741ba5dbf3
Trevor Dilley <trevor@twentyideas.com>
2018-04-19 22:57:50 -0700

    Initial commit of elm app embedded in react app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import './main.css';
-import { Main } from './Main.elm';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
