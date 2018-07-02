03bf42a3a0f6f17bcf62177eb8e0cf206d041904
Trevor Dilley <trevor@twentyideas.com>
2018-07-01 17:04:37 -0700

    Migrated completely over to Vue now

---
+++ src/components/Viagrammer.vue
@@ -0,0 +1,19 @@
+<template>
+    <ViagrammerElm></ViagrammerElm>
+</template>
+
+<script>
+import * as ElmComponent from "./ElmBridge"
+
+export default {
+  name: 'Viagrammer',
+  components: {
+    'ViagrammerElm': ElmComponent(require('./elm/src/Main.elm').Main)
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
