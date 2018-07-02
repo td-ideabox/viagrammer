c1d39bb5162901646a9666e7ed66101aa6404fda
Trevor Dilley <trevor@twentyideas.com>
2018-07-01 18:18:09 -0700

    Integrated Viz.js, printed stuff to string

--- src/components/Viagrammer.vue
+++ src/components/Viagrammer.vue
@@ -1,12 +1,26 @@
 <template>
+  <div>
     <ViagrammerElm></ViagrammerElm>
+  </div>
 </template>
 
 <script>
 import * as ElmComponent from "./ElmBridge"
-
+import Viz from "viz.js"
+import {Module, render} from "viz.js/full.render.js"
 export default {
   name: 'Viagrammer',
+  methods: {
+    vizJson: (e) => {
+      let v = new Viz( {Module, render});
+      v.renderJSONObject("digraph { a -> b; b->c; c -> a;}").then(result => {
+        console.log(result)
+        this.vizJson = result
+      }).catch(err => {
+        this.vizJson = err
+      })
+    }
+  },
   components: {
     'ViagrammerElm': ElmComponent(require('./elm/src/Main.elm').Main)
   }
