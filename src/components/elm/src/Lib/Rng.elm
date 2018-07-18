4a44abee280d04b5421a3e10e5671321414369b5
Trevor Dilley <trevor@twentyideas.com>
2018-07-18 16:22:20 -0700

    Destructure Rng

--- src/components/elm/src/Lib/Rng.elm
+++ src/components/elm/src/Lib/Rng.elm
@@ -1,25 +1,9 @@
 module Rng exposing (..)
 
-import Tuple exposing (first, second)
-
-
 --- Use the fibonnacci sequence as a random number
 --- generator
 
 
 next : ( Int, Int ) -> ( Int, Int )
-next rng =
-    let
-        a =
-            first rng
-
-        b =
-            second rng
-
-        a2 =
-            b
-
-        b2 =
-            (a + b) % 1000
-    in
-        ( a2, b2 )
+next ( a, b ) =
+    ( b, (a + b) % 1000 )
